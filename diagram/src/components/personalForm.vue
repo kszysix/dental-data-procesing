09400df3d0946ae82de345a906695f1581d507ea
Zosia <fraszosia@gmail.com>
2020-12-05 16:42:27 +0100

    diagram visualization initial commit

---
+++ diagram/src/components/personalForm.vue
@@ -0,0 +1,91 @@
+<template>
+<div>
+    <b-form class="ml-3 mr-3 personal-form round-corner">
+
+        <label class='h4 mb-4'>DANE OSOBOWE</label>
+
+        <div class="row">
+            <div class="col sm">
+                <label for="first-name">Pierwsze imię</label>
+                <b-form-input id="first-name" type="text"></b-form-input>
+            </div>
+            <div class="col sm">
+                <label for="second-name">Drugie imię</label>
+                <b-form-input id="second-name" type="text"></b-form-input>
+            </div>
+            <div class="col sm">
+                <label for="surname">Nazwisko</label>
+                <b-form-input id="surname" type="text"></b-form-input>
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-2">
+            <div class="col sm">
+                <label for="birth-date">Data urodzenia</label>
+                <b-form-input id="pesel" type="date"></b-form-input>
+            </div>
+            <div class="col sm">
+                <label for="pesel">Pesel</label>
+                <b-form-input id="pesel" type="number"></b-form-input>
+            </div>
+            <div class="col sm">
+                <label for="gender">Płeć</label>
+                <b-form-radio-group id="gender">
+                <b-form-radio value="male">Mężczyzna</b-form-radio>
+                <b-form-radio value="female">Kobieta</b-form-radio>
+                </b-form-radio-group>
+            </div>
+        </div>
+
+    </b-form>
+
+    <b-form class="ml-3 mr-3 personal-form round-corner">
+
+        <label class='h4 mb-4'>DANE KONTAKTOWE</label>
+
+        <div class="row">
+            <div class="col sm">
+                <label for="phone-number">Numer telefonu</label>
+                <b-form-input id="phone-number" type="text"></b-form-input>
+            </div>
+            <div class="col sm">
+                <label for="e-mail">E-mail</label>
+                <b-form-input id="e-mail" type="text"></b-form-input>
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-2">
+            <div class="col sm">
+                <label for="street">Ulica</label>
+                <b-form-input id="street" type="text"></b-form-input>
+            </div>
+            <div class="col sm">
+                <label for="address-1">Numer domu</label>
+                <b-form-input id="address-1" type="number"></b-form-input>
+            </div>
+            <div class="col sm">
+                <label for="address-2">Numer lokalu</label>
+                <b-form-input id="address-2" type="number"></b-form-input>
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-2">
+            <div class="col sm">
+                <label for="zip-code">Kod pocztowy</label>
+                <b-form-input id="zip-code" type="text"></b-form-input>
+            </div>
+            <div class="col sm">
+                <label for="city">Miejscowość</label>
+                <b-form-input id="city" type="text"></b-form-input>
+            </div>
+        </div>
+          
+    </b-form>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ diagram/src/components/tooth.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+        <div class="text-center mt-2 ml-2 mr-2 mb-2">
+            <svg
+                class='tooth'
+                viewBox="0 0 100 100"
+                role="group"
+                height="100" width="100"
+                >
+                <g>
+                    <path
+                        class='clickable-tooth'
+                        @click="changecolor('A')"
+                        :d="toothShape.A"
+                        :fill="color.A"/>
+                    <path
+                        class='clickable-tooth'
+                        @click="changecolor('B')"
+                        :d="toothShape.B"
+                        :fill="color.B"/>
+                    <path
+                        class='clickable-tooth'
+                        @click="changecolor('C')"
+                        :d="toothShape.C"
+                        :fill="color.C"/>
+                    <path
+                        class='clickable-tooth'
+                        @click="changecolor('D')"
+                        :d="toothShape.D"
+                        :fill="color.D"/>
+                    
+                    <path
+                        class='clickable-tooth'
+                        @click="changecolor('E')"
+                        :d="toothShape.E"
+                        :fill="color.E"/>
+                </g>
+            </svg>
+        </div>
+    </div>
+</template>
+
+<script>
+import { SQUARE_TOOTH_SHAPE, RECTANGLE_TOOTH_SHAPE } from './../assets/dentalData'
+
+export default {
+    name: 'tooth',
+    props: ['shape'],
+    data() {
+        return {
+            colors: ["lightseagreen", "lightsalmon", "darkslategray", "peru", "peachpuff", "gray"],
+            n1: 0,
+            n2: 0,
+            n3: 0,
+            n4: 0,
+            n5: 0,
+            color: { A: 'gray', B: 'gray', C: 'gray', D: 'gray', E: 'gray' },
+            toothShape: null
+        }
+    },
+    created() {
+        if (this.shape == 'square') this.toothShape = SQUARE_TOOTH_SHAPE
+        else this.toothShape = RECTANGLE_TOOTH_SHAPE
+    },
+    methods: {
+        changecolor(tooth) {
+            if (tooth == 'A') {
+                this.color.A = this.colors[this.n1]
+                this.n1 = (this.n1 + 1) % 6
+            }
+            else if (tooth == 'B') {
+                this.color.B = this.colors[this.n2]
+                this.n2 = (this.n2 + 1) % 6
+            }
+            else if (tooth == 'C') {
+                this.color.C = this.colors[this.n3]
+                this.n3 = (this.n3 + 1) % 6
+            }
+            else if (tooth == 'D') {
+                this.color.D = this.colors[this.n4]
+                this.n4 = (this.n4 + 1) % 6
+            }
+            else if (tooth == 'E') {
+                this.color.E = this.colors[this.n5]
+                this.n5 = (this.n5 + 1) % 6
+            }
+        },
+    },
+}
+
+</script>
